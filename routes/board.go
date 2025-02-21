8647c67e59a6c90582af6117e2f4108838cef787
sundo-kim <sdkim1@cloit.com>
2025-02-21 16:45:58 +0900

    게시판 구조 개선 및 라우터 분리
    - routes 패키지 추가 및 핸들러 분리
    - 게시판 타입 검증 로직 추가
    - EnableGzip 옵션 제거
    - 환경 변수 ALLOWED_BOARDS 추가

--- routes/board.go
+++ routes/board.go
@@ -48,23 +48,3 @@ func HandleBoardAPI(c *fiber.Ctx) error {
 		"type": boardType,
 	})
 }
-
-// SSR 라우트 핸들러
-func HandleFreeBoardSSR(c *fiber.Ctx) error {
-	// 게시글 데이터 조회
-	posts, err := GetPosts(/* 필요한 파라미터 */)
-	if err != nil {
-		return c.Status(500).SendString("서버 오류가 발생했습니다")
-	}
-
-	// 템플릿 렌더링
-	return c.Render("free", fiber.Map{
-		"Title": "자유게시판",
-		"Posts": posts,
-	})
-}
-
-// API 라우트 핸들러 (기존 코드 유지)
-func HandleFreeBoardAPI(c *fiber.Ctx) error {
-	// 기존 API 로직
-} 
